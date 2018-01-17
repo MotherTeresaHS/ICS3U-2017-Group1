2134a8a16165a4308fe8524db093f4d0bef643c5
Kay Lin <lamchuenkei@gmail.com>
2018-01-17 12:38:20 -0500

    Update level completed scenes

--- level_completed_scene.py
+++ level_completed_scene.py
@@ -5,17 +5,46 @@
 
 from scene import *
 import ui
-
+from level_1 import *
 
 class LevelCompletedScene(Scene):
     def setup(self):
         # this method is called, when user moves to this scene
         
+        self.size_of_screen_x = self.size.x
+        self.size_of_screen_y = self.size.y
+        self.screen_center_x = self.size_of_screen_x/2
+        self.screen_center_y = self.size_of_screen_y/2
+        
         # add background color
-        self.background = SpriteNode(position = self.size / 2, 
-                                     color = 'green', 
+        self.background = SpriteNode('./assets/sprites/completed_scene_background.JPG',
+                                     position = self.size/2, 
                                      parent = self, 
                                      size = self.size)
+                                     
+        home_button_position = Vector2()
+        home_button_position.x = self.screen_center_x + 100
+        home_button_position.y = self.screen_center_y - 100
+        self.home_button = SpriteNode('./assets/sprites/home_button.JPG',
+                                            parent = self,
+                                            position = home_button_position,
+                                            scale = 0.2)               
+                                     
+        retry_button_position = Vector2()
+        retry_button_position.x = self.screen_center_x + 100
+        retry_button_position.y = self.screen_center_y + 100
+        self.retry_button = SpriteNode('./assets/sprites/retry_button.JPG',
+                                          parent = self,
+                                          position = retry_button_position,                                                               
+                                          scale = 0.1)   
+        
+        next_button_position = Vector2()
+        next_button_position.x = self.screen_center_x + 100
+        next_button_position.y = self.screen_center_y + 250
+        self.next_button = SpriteNode('./assets/sprites/next_button.JPG',
+                                           parent = self,
+                                           position = next_button_position,
+                                           scale = 0.1)  
     
     def update(self):
         # this method is called, hopefully, 60 times a second
@@ -31,7 +60,17 @@ class LevelCompletedScene(Scene):
     
     def touch_ended(self, touch):
         # this method is called, when user releases a finger from the screen
-        pass
+        
+        # if retry button is pressed, restart the game
+            
+        # if home button is pressed, goto MainMenuScene scene
+        #if self.home_button.frame.contains_point(touch.location):
+            #self.present_modal_scene(MainMenuScene())
+        
+        # if next button is pressed, goto next level
+        # if self.next_button.frame.contains_point(touch.location):
+             #self.present_modal_scene(Level_2_scene)
+             
     
     def did_change_size(self):
         # this method is called, when user changes the orientation of the screen
